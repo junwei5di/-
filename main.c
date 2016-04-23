74cd9bfc53f6d99b52971a770f06595182d53d15
zhoujunwei <875839890@qq.com>
2016-04-23 15:49:19 +0800

    闰年

---
+++ main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  new
+//
+//  Created by qingyun on 16/3/9.
+//  Copyright © 2016年 qingyun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    int i;
+    scanf("%d",&i);
+    if (i%4==0 && i/100%4==0) {
+        printf("%d年是闰年\n",i);
+    }else{
+        printf("%d年是平年\n",i);
+    }
+}
